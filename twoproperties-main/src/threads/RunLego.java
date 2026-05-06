5755f07107a6b30fdc6218351e0fe669f632af29
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-05-06 16:52:12 +0300

    update RunLego.java

--- twoproperties-main/src/threads/RunLego.java
+++ twoproperties-main/src/threads/RunLego.java
@@ -1,26 +1,71 @@
 package threads;
 
-
-import data.*;
+import data.Robot;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 
-public class RunLego implements Runnable{
-    UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.B);
-	UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.C);
+public class RunLego implements Runnable {
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		while (Robot.getRun()==1) {
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			motorA.setPower(Robot.turnRight());
-			motorB.setPower(Robot.turnLeft());
-		}
-	}
+    // Left and right driving motors
+    UnregulatedMotor leftMotor = new UnregulatedMotor(MotorPort.B);
+    UnregulatedMotor rightMotor = new UnregulatedMotor(MotorPort.C);
 
+    @Override
+    public void run() {
+
+        while (Robot.isProgramRunning()) {
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            int run = Robot.getRun();
+            int speed = Robot.getSpeed();
+            int turn = Robot.getTurn();
+
+            leftMotor.setPower(speed);
+            rightMotor.setPower(speed);
+
+            if (run == 0) {
+                stopMotors();
+            }
+
+            // Forward
+            else if (turn == 0) {
+                leftMotor.forward();
+                rightMotor.forward();
+            }
+
+            // Left
+            else if (turn == 1) {
+                leftMotor.backward();
+                rightMotor.forward();
+            }
+
+            // Right
+            else if (turn == 2) {
+                leftMotor.forward();
+                rightMotor.backward();
+            }
+
+            // Backward
+            else if (turn == 3) {
+                leftMotor.backward();
+                rightMotor.backward();
+            }
+
+            else {
+                stopMotors();
+            }
+        }
+
+        stopMotors();
+    }
+
+    private void stopMotors() {
+        leftMotor.stop();
+        rightMotor.stop();
+    }
 }
